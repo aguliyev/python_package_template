c9e74d1ad1bd8574d57e780604e44d1b28e8c1e8
Anar Guliyev <anar@trialspark.com>
2023-01-26 21:20:47 +0400

    test coverage

--- etc/test/entrypoint.sh
+++ etc/test/entrypoint.sh
@@ -4,4 +4,5 @@ isort . && \
 black --extend-exclude=docs . && \
 mypy python_package_template && \
 flake8 python_package_template tests && \
-pytest
+coverage run -m pytest && \
+coverage report --show-missing --skip-covered --fail-under=100
