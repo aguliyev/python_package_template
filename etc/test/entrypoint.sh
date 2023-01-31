b9121af02b246cadafc53e1c8ea5343d0955b762
Anar Guliyev <anar@trialspark.com>
2023-01-31 10:12:37 -0600

    feat: make test should fail, so dont put if after the tests

--- etc/test/entrypoint.sh
+++ etc/test/entrypoint.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-isort . && \
-black --extend-exclude=docs . && \
-mypy python_package_template && \
-flake8 python_package_template tests && \
-coverage run -m pytest && \
-coverage report --show-missing --skip-covered --fail-under=100
 if [ $COVERAGE_REPORT ]; then
+    coverage run -m pytest && \
+    coverage report --show-missing --skip-covered --fail-under=100
     coverage xml
     coverage json
     coverage html
+else
+    isort . && \
+    black --extend-exclude=docs . && \
+    mypy python_package_template && \
+    flake8 python_package_template tests && \
+    coverage run -m pytest && \
+    coverage report --show-missing --skip-covered --fail-under=100
 fi
